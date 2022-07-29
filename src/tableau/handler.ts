606a7c22c04d0bc848511b6096934738fd6392ec
SnO2WMaN <me@sno2wman.net>
2022-07-30 03:58:14 +0900

    add chromium(pptr) support

--- src/tableau/handler.ts
+++ src/tableau/handler.ts
@@ -1,5 +1,20 @@
+import chromium from "chrome-aws-lambda";
 import { NextApiHandler } from "next";
 
 export const handler: NextApiHandler = async (req, res) => {
-  res.status(200).json({});
+  const browser = await chromium.puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    args: chromium.args,
+    headless: true,
+    defaultViewport: { ...chromium.defaultViewport, width: 568, height: 164 },
+  });
+  const page = await browser.newPage();
+  await page.goto("https://example.com", { waitUntil: "networkidle0" });
+  const image = await page.screenshot({ type: "png" });
+
+  res.setHeader("X-Robots-Tag", "noindex");
+  res.setHeader("Link", `<https://example.com>; rel="canonical"`);
+  res.setHeader("Content-Type", "image/png");
+  res.setHeader("Cache-Control", "max-age=86400, public, stale-while-revalidate");
+  res.send(image);
 };
