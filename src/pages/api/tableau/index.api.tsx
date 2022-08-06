01fde5f9029a4319c5ed4591421bb8879b14df47
SnO2WMaN <me@sno2wman.net>
2022-08-06 12:41:27 +0900

    idontknowwhy

--- src/pages/api/tableau/index.api.tsx
+++ src/pages/api/tableau/index.api.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable @next/next/no-head-element */
-
 import chromium from "chrome-aws-lambda";
 import { NextApiHandler } from "next";
 
@@ -31,27 +30,35 @@ const handler: NextApiHandler = async (req, res) => {
   // }
   // const { branch, formula, valid } = await apiRes.json<SolveApiResult>();
 
-  const browser = await chromium.puppeteer.launch({
-    executablePath: await chromium.executablePath,
-    args: chromium.args,
-    headless: true,
-    ignoreDefaultArgs: ["--disable-extensions"],
-    defaultViewport: { ...chromium.defaultViewport },
-  });
-  const page = await browser.newPage();
-  // const html = ReactDOMServer.renderToStaticMarkup(<HtmlTemplate branch={branch} formula={formula} valid={valid} />);
-  await page.goto("https://example.com", { waitUntil: "networkidle0" });
-  // await page.goto;
-  const image = await page.screenshot({ type: "png" });
+  try {
+    const browser = await chromium.puppeteer.launch({
+      executablePath: await chromium.executablePath,
+      args: chromium.args,
+      headless: true,
+      ignoreDefaultArgs: ["--disable-extensions"],
+      defaultViewport: { ...chromium.defaultViewport },
+    });
+    const page = await browser.newPage();
+    // const html = ReactDOMServer.renderToStaticMarkup(<HtmlTemplate branch={branch} formula={formula} valid={valid} />);
+    await page.goto("https://example.com", { waitUntil: "networkidle0" });
+    // await page.goto;
+    const image = await page.screenshot({ type: "png" });
 
-  // res.setHeader("Content-Type", "text/html; charset=UTF-8");
-  // res.send(html);
-  // return;
+    // res.setHeader("Content-Type", "text/html; charset=UTF-8");
+    // res.send(html);
+    // return;
 
-  res.setHeader("Content-Type", "image/png");
-  res.setHeader("Cache-Control", "max-age=86400, public, stale-while-revalidate");
-  res.send(image);
-  return;
+    res.status(200);
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Cache-Control", "max-age=86400, public, stale-while-revalidate");
+    res.send(image);
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.end();
+    return;
+  }
 };
 
 export default handler;
